603159be1783d78855578aaa1fdf3ef6fc5aa905
tanner-perham <tanner.perham@gmail.com>
2021-02-17 21:59:11 +0800

    Learnt simple use of query selector to interact with DOM

--- JavaScript/JS_inBrowser-DOMandEvents/GuessMyNumber/script.js
+++ JavaScript/JS_inBrowser-DOMandEvents/GuessMyNumber/script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number!";
+
+console.log(document.querySelector(".score").textContent);
+
+document.querySelector(".number").textContent = 10;
+
+console.log(document.querySelector(".guess").value);
