922901213d3462ee6320993322220e9cc79272bd
tanner-perham <tanner.perham@gmail.com>
2021-02-21 13:54:40 +0800

    added reset game function with new game button click event. Broke out key page elements into variables

--- JavaScript/JS_inBrowser-DOMandEvents/PigGame/script.js
+++ JavaScript/JS_inBrowser-DOMandEvents/PigGame/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const btnNewGame = document.querySelector(".btn--new");
+const btnDiceRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const players = document.querySelectorAll(".player");
+
+for (let i = 0; i < players.length; i++) {
+  console.log(players[i].querySelector(".score"));
+}
+
+const resetGame = function () {
+  let currentPlayer = 0;
+  let player1Score = 0;
+  let player2Score = 0;
+  let player1Current = 0;
+  let player2Current = 0;
+  console.log(
+    currentPlayer,
+    player1Score,
+    player2Score,
+    player1Current,
+    player2Current
+  );
+};
+const passTurn = function () {};
+
+resetGame();
+btnNewGame.addEventListener("click", resetGame);
