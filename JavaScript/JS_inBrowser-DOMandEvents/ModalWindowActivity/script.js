8aba71f84f5280da65598c2fd05a216f30741b97
tanner-perham <tanner.perham@gmail.com>
2021-02-21 12:55:35 +0800

    refactored code to use openModal and closeModal functions

--- JavaScript/JS_inBrowser-DOMandEvents/ModalWindowActivity/script.js
+++ JavaScript/JS_inBrowser-DOMandEvents/ModalWindowActivity/script.js
@@ -5,20 +5,19 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener("click", function () {
-    console.log(btnsShowModal[i].textContent);
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  btnsShowModal[i].addEventListener("click", openModal);
 }
 
-overlay.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-
-btnCloseModal.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener("click", closeModal);
