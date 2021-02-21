55369d5b77be1a0c0f271ad70b07215ac0b265a3
tanner-perham <tanner.perham@gmail.com>
2021-02-21 12:45:33 +0800

    modal show and close functionality implemented

--- JavaScript/JS_inBrowser-DOMandEvents/ModalWindowActivity/script.js
+++ JavaScript/JS_inBrowser-DOMandEvents/ModalWindowActivity/script.js
@@ -4,3 +4,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", function () {
+    console.log(btnsShowModal[i].textContent);
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
