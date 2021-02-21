c0503336d47f4498e568fa01caede44b3e9489a0
tanner-perham <tanner.perham@gmail.com>
2021-02-21 20:40:01 +0800

    implemented all features

--- JavaScript/JS_inBrowser-DOMandEvents/PigGame/script.js
+++ JavaScript/JS_inBrowser-DOMandEvents/PigGame/script.js
@@ -6,12 +6,7 @@ const btnHold = document.querySelector(".btn--hold");
 const dice = document.querySelector(".dice");
 const players = document.querySelectorAll(".player");
 
-for (let i = 0; i < players.length; i++) {
-  console.log(players[i].querySelector(".score"));
-}
-
 const resetGame = function () {
-  let currentPlayer = 0;
   let player1Score = 0;
   let player2Score = 0;
   let player1Current = 0;
@@ -37,13 +32,52 @@ const passTurn = function () {
   }
 };
 
+const updateScore = function (value) {
+  if (players[0].classList.contains("player--active")) {
+    if (value !== 1) {
+      players[0].querySelector(".score").textContent =
+        Number(players[0].querySelector(".score").textContent) + value;
+    } else {
+      players[0].querySelector(".score").textContent = 0;
+      passTurn();
+    }
+  } else {
+    if (value !== 1) {
+      players[1].querySelector(".score").textContent =
+        Number(players[1].querySelector(".score").textContent) + value;
+    } else {
+      players[1].querySelector(".score").textContent = 0;
+      passTurn();
+    }
+  }
+};
+
 const rollDice = function () {
   let diceValue = Math.trunc(Math.random() * 6) + 1;
   dice.setAttribute("src", `dice-${diceValue}.png`);
+  updateScore(diceValue);
+};
+
+const updateCurrent = function () {
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].classList.contains("player--active")) {
+      players[i].querySelector(".current-score").textContent =
+        Number(players[i].querySelector(".current-score").textContent) +
+        Number(players[i].querySelector(".score").textContent);
+    }
+  }
 };
 
 const hold = function () {
-  passTurn();
+  updateCurrent();
+  updateScore(1);
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].querySelector(".current-score").textContent > 100) {
+      let playerName = players[i].querySelector(".name").textContent;
+      btnDiceRoll.textContent = `${playerName} Wins!!!`;
+    }
+  }
+  //   passTurn();
 };
 
 resetGame();
