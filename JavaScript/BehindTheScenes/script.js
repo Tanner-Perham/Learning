992b1aa680a8540af7301617b86b2f3cdd4b1b69
tanner-perham <tanner.perham@gmail.com>
2021-02-27 11:52:02 +0800

    Began learning about use of the this keyword.

--- JavaScript/BehindTheScenes/script.js
+++ JavaScript/BehindTheScenes/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 const button = document.querySelector(".head-btn");
 const header = document.querySelector(".header");
 
@@ -11,3 +12,24 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
   return age;
 }
+*/
+
+// Using 'this' keyword
+console.log(this);
+
+const tanner = {
+  year: 1993,
+  calcAge: function () {
+    console.log(2021 - this.year);
+  },
+};
+tanner.calcAge();
+
+const ceily = {
+  year: 1996,
+};
+ceily.calcAge = tanner.calcAge;
+
+console.log(ceily);
+
+ceily.calcAge();
