566d2b4d76253bfd95ca549d553dba29abfa2b60
tanner-perham <tanner.perham@gmail.com>
2021-03-19 19:48:36 +0800

    finished matrix multiplication testing

--- MachineLearning/MatrixMultiplication/matrix_multiplication.f95
+++ MachineLearning/MatrixMultiplication/matrix_multiplication.f95
@@ -2,11 +2,27 @@
 program matrix_multplication
     !Get dimensions of matrices
     integer :: i, j, k
+    integer :: x, y, z
     integer, allocatable :: A(:,:), B(:,:), C(:,:)
-    i = 2   !rows of A
-    j = 3   !cols of A & rows of B
-    k = 2   !cols of B
-    allocate(A(i,j), B(j,k), C(i, k))
+    x = 1   !rows of A
+    y = 16   !cols of A & rows of B
+    z = 10000   !cols of B
+    allocate(A(x,y), B(y,z), C(x, z))
+    do i = 1,x
+        do j = 1,y
+            A(i, j) = 2
+        end do
+    end do
 
+    do i = 1,y
+        do j = 1,z
+            B(i, j) = 3
+        end do
+    end do
+
+    C = matmul(A, B)
+
+    print*, C, 'done!'
+    
 
 end program matrix_multplication
