cc31e02873fe2004b501f86d432fc0ebf1a8d2c1
tanner-perham <tanner.perham@gmail.com>
2021-04-04 06:10:41 +0800

    Got the graph module to a stage where I can search the graph tree for a list of words.

--- JavaScript/WordSleuthApp/modules/graph.js
+++ JavaScript/WordSleuthApp/modules/graph.js
@@ -1,4 +1,4 @@
-const testData = [
+const testData1 = [
   "Alder",
   "Apple",
   "Ash",
@@ -31,9 +31,31 @@ const testData = [
   "Yew",
 ];
 
+const testData2 = [
+  "Alder",
+  "Apple",
+  "Ash",
+  "Aspen",
+  "Birch",
+  "Buckthorn",
+  "Cedar",
+  "Cherry",
+  "Chestnut",
+  "Chinkapin",
+  "Cottonwood",
+  "Cypress",
+  "Tanner",
+  "Jade",
+  "Chair",
+  "Lamp",
+];
+
+const smallTest = ["ab", "bab", "bc", "bca", "caa"];
+
 class Node {
-  constructor(name) {
+  constructor(name, level = 0) {
     this.name = name;
+    this.level = level;
     this.parents = [];
     this.children = [];
   }
@@ -45,28 +67,49 @@ class Node {
   addParent(parent) {
     this.parents.push(parent);
   }
+
+  getChildren() {
+    return this.children;
+  }
+
+  // isChildOf(){}
 }
 
 class Graph {
   constructor() {
     this.nodes = [];
     this.edges = [];
+    this.addNode("root");
   }
 
-  addNode(name) {
-    this.nodes.push(new Node(name));
+  addNode(name, level) {
+    this.nodes.push(new Node(name, level));
   }
-  getNode(name) {
-    return this.nodes.find((letter) => letter.name === name);
+  getNode(name, level = 0) {
+    return this.nodes.find(
+      (letter) => letter.name === name && letter.level === level
+    );
   }
 
-  addEdge(parent, child) {
-    const parentNode = this.getNode(parent);
-    const childNode = this.getNode(child);
-    if (!this.edges.includes(`${parent} - ${child}`)) {
+  addEdge(parent, child, level) {
+    const parentNode = this.getNode(parent, level);
+    let childNode;
+    if (this.getNode(child, parentNode.level + 1)) {
+      childNode = this.getNode(child, parentNode.level + 1);
+    } else {
+      this.addNode(child, parentNode.level + 1);
+      childNode = this.getNode(child, parentNode.level + 1);
+    }
+    if (
+      !this.edges.includes(
+        `[${parent}, ${parentNode.level}] - [${child}, ${childNode.level}]`
+      )
+    ) {
       parentNode.addChild(childNode);
       childNode.addParent(parentNode);
-      this.edges.push(`${parent} - ${child}`);
+      this.edges.push(
+        `[${parent}, ${parentNode.level}] - [${child}, ${childNode.level}]`
+      );
     } else return;
   }
 
@@ -86,24 +129,41 @@ function addWordToGraph(word) {
   for (let [i, j] = [0, 2]; j <= word.length; [i++, j++]) {
     wordDuples.push([...word.toUpperCase().slice(i, j)]);
   }
-  //   console.log(wordDuples);
 
-  graph.addEdge("root", wordDuples[0][0]);
+  graph.addEdge("root", wordDuples[0][0], 0);
   wordDuples.map((arr, i) => {
-    graph.addEdge(...arr);
+    graph.addEdge(...arr, i + 1);
   });
-  graph.addEdge(wordDuples.pop()[1], "end");
+  graph.addEdge(wordDuples.pop()[1], "end", word.length);
 }
 
-const graph = new Graph(true);
-graph.addNode("root");
-graph.addNode("end");
+const graph = new Graph();
 
-const nodeList = [
-  ...new Set(testData.reduce((acc, cur) => acc + cur).toUpperCase()),
-];
-nodeList.map((node) => graph.addNode(node));
+testData1.map((word) => addWordToGraph(word));
 
-testData.map((word) => addWordToGraph(word));
+function searchGraph(word) {
+  const wordArr = [...word.toUpperCase(), "end"];
 
-console.log(graph.nodes.filter(({ name }) => name === "root"));
+  let searchPoint = graph.nodes.find((node) => node.name === "root");
+  const results = [searchPoint];
+
+  wordArr.map((cur) => {
+    if (
+      searchPoint &&
+      searchPoint.children?.filter((node) => {
+        node.name === cur;
+      })
+    ) {
+      searchPoint = searchPoint.children.find((node) => node.name === cur);
+    } else {
+      searchPoint = undefined;
+    }
+    results.push(searchPoint);
+  });
+
+  return results.includes(undefined)
+    ? `${word} not in tree`
+    : `${word} is in the tree!`;
+}
+
+console.log(testData2.map((w) => searchGraph(w)));
